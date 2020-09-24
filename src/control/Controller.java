3c1ed41b00d17c6224cf4d3eac11261e3844156d
Iker <theofficialdork@hotmail.com>
2020-09-24 08:30:23 +0200

    Inicio de proyecto DIN

---
+++ src/control/Controller.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import model.Model;
+import view.View;
+
+/**
+ *
+ * @author iker
+ */
+public class Controller {
+    public void run(View view, Model model){
+        
+    }
+}
---
+++ src/model/ModelImplementation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author iker
+ */
+public class ModelImplementation implements Model {
+    /**
+     * 
+     * @return Return the message.
+     */
+    public String getGreeting(){
+        return null;
+    }
+}
